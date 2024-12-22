dc9d9457ee8c18603bef26599edce3ec85cbcf7a
Ashma Begum Pathan <pathanashmabegum@gmail.com>
2024-12-22 11:29:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-// Toggle FAQ Answer Visibility
-document.querySelectorAll('.faq-question').forEach(button => {
-    button.addEventListener('click', () => {
-      const faqItem = button.parentElement;
-      faqItem.classList.toggle('open');
-    });
-  });
-  
